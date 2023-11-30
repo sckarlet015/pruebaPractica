6cd8b2144e0c23d9e00dc6e7e6d0bbd42cc9c6d7
sckarlet015 <sckarlet015@gmail.com>
2023-11-30 17:04:38 -0600

    correccion ES

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+import { listen } from './src/app.js';
+import { conn } from './src/db.js';
 
 const port = process.env.PORT || 3001
 
 conn.sync({ force: true }).then(async() => {  
-  server.listen(port, () => {
+  listen(port, () => {
     console.log(`Server on port ${port}`);
   });
 });
